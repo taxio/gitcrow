b728f39613ca7dfdf6a064f76f6c28f0d17f958e
taxio <taxio.2029@gmail.com>
2019-05-02 13:33:27 +0900

    add test template

---
+++ app/server/gitcrow_server_test.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"context"
+	api_pb "github.com/taxio/gitcrow/api"
+	"testing"
+)
+
+func TestGitcrowServiceServerImpl_CloneRepositories(t *testing.T) {
+	svr := NewGitcrowServiceServer()
+
+	ctx := context.Background()
+	req := &api_pb.CloneRepositoriesRequest{}
+
+	resp, err := svr.CloneRepositories(ctx, req)
+
+	t.SkipNow()
+
+	if err != nil {
+		t.Errorf("returned an error %v", err)
+	}
+
+	if resp == nil {
+		t.Error("response should not nil")
+	}
+}
+
+func TestGitcrowServiceServerImpl_DownloadRepositories(t *testing.T) {
+	svr := NewGitcrowServiceServer()
+
+	ctx := context.Background()
+	req := &api_pb.DownloadRepositoriesRequest{}
+
+	resp, err := svr.DownloadRepositories(ctx, req)
+
+	t.SkipNow()
+
+	if err != nil {
+		t.Errorf("returned an error %v", err)
+	}
+
+	if resp == nil {
+		t.Error("response should not nil")
+	}
+}
