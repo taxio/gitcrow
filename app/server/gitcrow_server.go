28c5ca0d121c025d0f6c0d8ffa2b17ebcc6b06a6
taxio <taxio.2029@gmail.com>
2019-05-02 15:27:41 +0900

    server impl

--- app/server/gitcrow_server.go
+++ app/server/gitcrow_server.go
@@ -3,9 +3,8 @@ package server
 import (
 	"context"
 	"github.com/izumin5210/grapi/pkg/grapiserver"
+	"github.com/k0kubun/pp"
 	api_pb "github.com/taxio/gitcrow/api"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 type GitcrowServiceServer interface {
@@ -21,11 +20,15 @@ type gitcrowServiceServerImpl struct {
 }
 
 func (s *gitcrowServiceServerImpl) CloneRepositories(ctx context.Context, req *api_pb.CloneRepositoriesRequest) (*api_pb.CloneRepositoriesResponse, error) {
-	// TODO: Not yet implemented
-	return nil, status.Error(codes.Unimplemented, "Not implemented")
+	//// TODO: Not yet implemented
+	//return nil, status.Error(codes.Unimplemented, "Not implemented")
+	pp.Println(req)
+	return &api_pb.CloneRepositoriesResponse{Message: "request accepted"}, nil
 }
 
 func (s *gitcrowServiceServerImpl) DownloadRepositories(ctx context.Context, req *api_pb.DownloadRepositoriesRequest) (*api_pb.DownloadRepositoriesResponse, error) {
-	// TODO: Not yet implemented
-	return nil, status.Error(codes.Unimplemented, "Not implemented")
+	//// TODO: Not yet implemented
+	//return nil, status.Error(codes.Unimplemented, "Not implemented")
+	pp.Println(req)
+	return &api_pb.DownloadRepositoriesResponse{Message: "request accepted"}, nil
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/protobuf v1.2.0
 	github.com/grpc-ecosystem/grpc-gateway v1.8.5
 	github.com/izumin5210/grapi v0.4.0
+	github.com/k0kubun/pp v3.0.1+incompatible
 	golang.org/x/net v0.0.0-20190311183353-d8887717615a
 	google.golang.org/genproto v0.0.0-20180817151627-c66870c02cf8
 	google.golang.org/grpc v1.20.1
