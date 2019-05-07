2d6efbb9e1390afd5ca683c60f8177c5f3ed4949
taxio <taxio.2029@gmail.com>
2019-05-07 21:50:57 +0900

    save user dir

--- infra/user_store.go
+++ infra/user_store.go
@@ -2,17 +2,49 @@ package infra
 
 import (
 	"context"
+	"fmt"
+	"github.com/pkg/errors"
 	"github.com/taxio/gitcrow/domain/model"
 	"github.com/taxio/gitcrow/domain/repository"
+	"google.golang.org/grpc/grpclog"
+	"os"
+	"path/filepath"
 )
 
-type userStoreImpl struct {}
-
-func NewUserStore() repository.UserStore {
-	return &userStoreImpl{}
+type userStoreImpl struct {
+	baseDir string
 }
 
-func (s *userStoreImpl) Save(ctx context.Context, filename string, data []byte) error {
+func NewUserStore(baseDir string) repository.UserStore {
+	return &userStoreImpl{
+		baseDir: baseDir,
+	}
+}
+
+func (s *userStoreImpl) Save(ctx context.Context, username, projectName, filename string, data []byte) error {
+	// create path
+	// TODO: validate for traverse
+	p := filepath.Join(s.baseDir, username, projectName, filename)
+	grpclog.Infof("save: %s\n", p)
+
+	// create file
+	file, err := os.OpenFile(p, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("failed to create %s", p))
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			grpclog.Errorln(err)
+		}
+	}()
+
+	// write zip data
+	_, err = file.Write(data)
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("failed to write %s", p))
+	}
+
 	return nil
 }
 
